9e2fc0bb883253d13f7252e6ee82b0417c3e7913
VJ09743 <viraj.verma0616@gmail.com>
2025-10-11 17:16:00 +0200

    Made dropdown centered and above navbar

--- app/navigation.js
+++ app/navigation.js
@@ -21,14 +21,14 @@ export async function getServerSideProps(context) {
 export default function NaviBar({ isDesktop }) {
 
     return(
-        <nav className={` flex bg-black/25 group hover:p-5 p-2 gap-2 justify-center hover:gap-6 transition-all duration-100  rounded-full backdrop-blur-sm top-5 hover:w-98/100 fixed z-50  ${isDesktop?`not-hover:bg-black/20`:``}`}>
+        <nav className={` flex bg-black/25 group hover:p-5 p-2 gap-2 justify-items-center justify-center hover:gap-6 transition-all duration-100  rounded-full backdrop-blur-sm top-5 hover:w-98/100 fixed z-50  ${isDesktop?``:`not-hover:bg-black/15`}`}>
             {linkBut("/", "Home")}
             <details className="relative">
                 <summary className={confbut + " cursor-pointer list-none"}>
                     Games▽
                 </summary>
 
-                <div className={`absolute left-0 mt-2 items-center gap-6 bg-black/20 rounded-lg shadow-lg flex flex-col p-2 z-50 ${isDesktop?`not-group-hover:collapse `:``}`}>
+                <div className={`absolute mt-2 items-center gap-6 left-1/2 -translate-x-1/2 bg-black/20 rounded-lg shadow-lg flex flex-col p-2 z-51 ${isDesktop?``:`not-group-hover:collapse`}`}>
                     {linkBut("/games/cookie", "Clicker")}
                 </div>
             </details>
