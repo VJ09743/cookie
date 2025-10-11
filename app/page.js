0e346fc113fc0f7ebb9dc44bdeaa62e66b6ec2b6
VJ09743 <viraj.verma0616@gmail.com>
2025-10-11 17:58:27 +0200

    Made buttons over navbar

--- app/page.js
+++ app/page.js
@@ -9,7 +9,11 @@ export default async function Home() {
 
     return (
 
-        <div className="flex bg-gradient-to-br from-blue-800 to-green-800 h-full  justify-center">
+        <div className="flex bg-gradient-to-br from-blue-800 to-green-800 h-screen  justify-center"   style={{
+
+            marginTop: "calc(-1 * env(safe-area-inset-top))",
+            marginBottom: "calc(-1 * env(safe-area-inset-bottom))",
+        }}>
 
             <NaviBar />
             <main className="text-white bg-neutral-400/20 h-fit rounded-3xl p-10 mt-25 w-95/100 overflow-auto">
