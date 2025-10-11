a2d763c37fd44f8a5dbe4aafb9e3184fb0eef3be
VJ09743 <viraj.verma0616@gmail.com>
2025-10-11 20:40:20 +0200

    Checkin

--- app/navigation.js
+++ app/navigation.js
@@ -2,22 +2,26 @@
 import Link from "next/link";
 
 import MobileDetect from "mobile-detect";
+import {useEffect} from "react";
 const confbut = "group-hover:text-3xl text-1xl px-4 hover:bg-neutral-400/30  group-hover:px-8 py-3 rounded-full z-51 bg-neutral-400/20 text-white/60 group-hover:text-white transition-all duration-100";
 export default  function NaviBar() {
+    let isDesktop = true;
+    useEffect(() => {
+        const md = new MobileDetect(navigator.userAgent);
+        const isMobile = !!md.mobile();
+        const isTablet = !!md.tablet();
+        isDesktop = (!isMobile && !isTablet);
+    }, []);
 
-    const md = new MobileDetect(navigator.userAgent);
-    const isMobile = !!md.mobile();
-    const isTablet = !!md.tablet();
-    let isDesktop = (!isMobile && !isTablet);
     return(
         <nav className={` flex bg-black/30 group hover:p-5 p-2 gap-2 justify-items-center justify-center hover:gap-6 transition-all duration-100  rounded-full backdrop-blur-sm top-5 hover:w-98/100 fixed z-50  ${isDesktop?`not-hover:bg-black/15`:``}`}>
-            {linkBut("/", (isDesktop?"Dek":"Monb"))}
-            <details className="relative">
+            {linkBut("/", "Home")}
+            <details className={`relative `}>
                 <summary className={confbut + " cursor-pointer list-none"}>
                     Games▽
                 </summary>
 
-                <div className={`absolute mt-2 items-center gap-6 left-1/2 -translate-x-1/2 bg-black/20 rounded-lg shadow-lg flex flex-col p-2 z-52 ${isDesktop?`not-group-hover:collapse`:``}`}>
+                <div className={`absolute mt-2 items-center gap-6 left-1/2 -translate-x-1/2 bg-black/20 rounded-lg shadow-lg flex flex-col p-2 z-52 ${isDesktop?`not-group-hover:collapse`:``} `}>
                     {linkBut("/games/cookie", "Clicker")}
                 </div>
             </details>
